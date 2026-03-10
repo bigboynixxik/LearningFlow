da5b0d7a78a29d56c6e20a7697b473e0bf0db1b9
Nixxik <v.gorbachev05@yandex.ru>
2026-03-10 22:16:47 +0700

    add ssr

--- cmd/learnflow/main.go
+++ cmd/learnflow/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"context"
+	"learningflow/internal/app"
+)
+
+func main() {
+	ctx := context.Background()
+	a := app.New(ctx)
+	a.Run(ctx)
+}
--- internal/transport/ssr/ssr.go
+++ internal/transport/ssr/ssr.go
@@ -1,10 +1,36 @@
 package ssr
 
 import (
+	"html/template"
 	"learningflow/internal/models"
+	"log/slog"
 	"net/http"
 )
 
+type HomeData struct {
+	Title    string
+	Subjects []models.Subject
+}
+
 func HandleHome(w http.ResponseWriter, r *http.Request) {
-	var subjects []models.Subject
+	subjects := []models.Subject{
+		{ID: 1, Name: "Математика"},
+		{ID: 2, Name: "Программирование на Go"},
+		{ID: 3, Name: "Английский язык"},
+		{ID: 4, Name: "Физика"},
+	}
+
+	data := HomeData{Subjects: subjects, Title: "Learning Flow"}
+
+	tmpl, err := template.ParseFiles("web/templates/index.html")
+	if err != nil {
+		slog.Error("ssr.HandleHome template error",
+			slog.String("err", err.Error()))
+		http.Error(w, "ssr.HandleHome template error", http.StatusInternalServerError)
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		slog.Error("ssr.HandleHome template error",
+			slog.String("err", err.Error()))
+		http.Error(w, "ssr.HandleHome template error", http.StatusInternalServerError)
+	}
 }
